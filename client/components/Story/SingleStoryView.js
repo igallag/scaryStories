311863e62223332ebf589c3c9b6291914662b5fb
Ian Gallgher <igallag93@gmail.com>
2019-11-07 16:51:00 -0500

    mapped state to props on the single movie and just need to find a way to access key

--- client/components/Story/SingleStoryView.js
+++ client/components/Story/SingleStoryView.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
 
 const SingleMovieView = props => {
+  console.log(props.stories, 'this is stories')
+  const currStory = props.stories[0]
   return (
     <div>
       <h1>Hey Y'all</h1>
+      <p>{currStory.title}</p>
     </div>
   )
 }
 
-export default SingleMovieView
+const mapStateToProps = state => {
+  return {
+    stories: state.story.stories
+  }
+}
+
+export default connect(mapStateToProps)(SingleMovieView)
