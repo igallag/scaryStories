3ef0fed08fc1e43d1c50f50d74c1207eda2dba25
Ian Gallgher <igallag93@gmail.com>
2019-11-07 16:24:02 -0500

    made the single movies have a link to a single movie view, still needs to be built

--- client/components/Story/SingleStory.js
+++ client/components/Story/SingleStory.js
@@ -5,9 +5,16 @@ import {Link} from 'react-router-dom'
 const SingleStory = props => {
   return (
     <div>
-      <img src={props.story.imageUrl} height="100" width="100" />
-      <h3>{props.story.title}</h3>
-      <p>{props.story.description}</p>
+      <Link to={`/stories/${props.story.slug}`}>
+        <img
+          src={props.story.imageUrl}
+          height="100"
+          width="100"
+          alt={props.story.imageAltText}
+        />
+        <h3>{props.story.title}</h3>
+        <p>{props.story.description}</p>
+      </Link>
     </div>
   )
 }
