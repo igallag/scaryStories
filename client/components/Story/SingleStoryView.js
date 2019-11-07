ce3579932b99cbb68611e799e2f037fdd98d61ca
Ian Gallgher <igallag93@gmail.com>
2019-11-07 16:43:50 -0500

    added a link to single movie view and need to get the state to props

---
+++ client/components/Story/SingleStoryView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const SingleMovieView = props => {
+  return (
+    <div>
+      <h1>Hey Y'all</h1>
+    </div>
+  )
+}
+
+export default SingleMovieView
--- client/routes.js
+++ client/routes.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 import {Login, Signup, UserHome} from './components'
 import {me} from './store'
 import AllStories from './components/Story/AllStoriesView'
+import SingleStoryView from './components/Story/SingleStoryView'
 
 /**
  * COMPONENT
@@ -23,6 +24,7 @@ class Routes extends Component {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route exact path="/stories" component={AllStories} />
+        <Route path="/stories/" component={SingleStoryView} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
