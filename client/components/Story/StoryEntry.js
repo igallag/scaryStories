d60cfd3eb69839cd1a28a67b263e24569d3ccbf9
Ian Gallgher <igallag93@gmail.com>
2019-11-12 11:59:34 -0500

    added ability to add story with a thunk in the story reducer

--- client/components/Story/StoryEntry.js
+++ client/components/Story/StoryEntry.js
@@ -1,48 +1,84 @@
 import React from 'react'
 
-const StoryEntry = props => {
-  return (
-    <div id="story-entry">
-      <form>
-        <div>
-          <label htmlFor="title">
-            <small>Title</small>
-          </label>
-          <input name="title" type="text" />
-        </div>
-        <div>
-          <label htmlFor="description">
-            <small>Description</small>
-          </label>
-          <input name="description" type="text" />
-        </div>
-        <div>
-          <label htmlFor="content">
-            <small>Content</small>
-          </label>
-          <input name="content" type="text" />
-        </div>
-        <div>
-          <label htmlFor="imageUrl">
-            <small>Image (optional)</small>
-          </label>
-          <input name="imageUrl" type="text" />
-        </div>
-        <div>
-          <label htmlFor="imageAlt">
-            <small>Image Alt Text (optional)</small>
-          </label>
-          <input name="imageAlt" type="text" />
-        </div>
-        <div>
-          <label htmlFor="tags">
-            <small>Tags (seperate with a comma and space)</small>
-          </label>
-          <input name="tags" type="text" />
-        </div>
-      </form>
-    </div>
-  )
+class StoryEntry extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      description: '',
+      content: '',
+      imageUrl: '',
+      imageAlt: '',
+      tags: []
+    }
+  }
+
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
+
+  // needs refactoring for this project
+  handleSubmit(evt) {
+    evt.preventDefault()
+
+    //   const res = await axios.post('/api/campuses', {
+    //       name: this.state.name,
+    //       address: this.state.address
+    //   });
+
+    this.props.addNewCampus(this.state)
+    this.setState({
+      name: '',
+      address: ''
+    })
+  }
+
+  render() {
+    return (
+      <div id="story-entry">
+        <form>
+          <div>
+            <label htmlFor="title">
+              <small>Title</small>
+            </label>
+            <input name="title" type="text" />
+          </div>
+          <div>
+            <label htmlFor="description">
+              <small>Description</small>
+            </label>
+            <input name="description" type="text" />
+          </div>
+          <div>
+            <label htmlFor="content">
+              <small>Content</small>
+            </label>
+            <input name="content" type="text" />
+          </div>
+          <div>
+            <label htmlFor="imageUrl">
+              <small>Image (optional)</small>
+            </label>
+            <input name="imageUrl" type="text" />
+          </div>
+          <div>
+            <label htmlFor="imageAlt">
+              <small>Image Alt Text (optional)</small>
+            </label>
+            <input name="imageAlt" type="text" />
+          </div>
+          <div>
+            <label htmlFor="tags">
+              <small>Tags (seperate with a comma and space)</small>
+            </label>
+            <input name="tags" type="text" />
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default StoryEntry
--- client/store/story.js
+++ client/store/story.js
@@ -8,9 +8,11 @@ let initialState = {
 
 // ACTION TYPES
 const GET_ALL_STORIES = 'GET_ALL_STORIES'
+const ADD_NEW_STORY = 'ADD_NEW_STORY'
 
 // ACTION CREATORS
 const getAllStories = stories => ({type: GET_ALL_STORIES, stories})
+const addNewStory = story => ({type: ADD_NEW_STORY, story})
 
 // THUNK CREATORS
 export const getAllStoriesThunk = () => {
@@ -24,12 +26,26 @@ export const getAllStoriesThunk = () => {
   }
 }
 
+export const addNewStoryThunk = story => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.post('api/stories', story)
+      dispatch(addNewStory(data))
+      history.push(`/stories/${data.slug}`)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
 // REDUCER
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ALL_STORIES:
       return {...state, stories: action.stories}
+    case ADD_NEW_STORY:
+      return {...state, stories: [...state.stories, action.story]}
     default:
       return state
   }
