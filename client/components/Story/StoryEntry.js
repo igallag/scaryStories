5105ae289655834213480aab1b594b0dd709509a
Ian Gallgher <igallag93@gmail.com>
2019-11-12 12:14:39 -0500

    added change and submit handlers and connected the compo needs testing

--- client/components/Story/StoryEntry.js
+++ client/components/Story/StoryEntry.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {addNewStoryThunk} from '../../store/story'
 
 class StoryEntry extends React.Component {
   constructor(props) {
@@ -11,6 +13,8 @@ class StoryEntry extends React.Component {
       imageAlt: '',
       tags: []
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange(evt) {
@@ -23,57 +27,86 @@ class StoryEntry extends React.Component {
   handleSubmit(evt) {
     evt.preventDefault()
 
-    //   const res = await axios.post('/api/campuses', {
-    //       name: this.state.name,
-    //       address: this.state.address
-    //   });
-
-    this.props.addNewCampus(this.state)
+    this.props.addNewStory(this.state)
     this.setState({
-      name: '',
-      address: ''
+      title: '',
+      description: '',
+      content: '',
+      imageUrl: '',
+      imageAlt: '',
+      tags: []
     })
   }
 
   render() {
     return (
       <div id="story-entry">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div>
             <label htmlFor="title">
               <small>Title</small>
             </label>
-            <input name="title" type="text" />
+            <input
+              name="title"
+              type="text"
+              value={this.state.title}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <label htmlFor="description">
               <small>Description</small>
             </label>
-            <input name="description" type="text" />
+            <input
+              name="description"
+              type="text"
+              value={this.state.description}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <label htmlFor="content">
               <small>Content</small>
             </label>
-            <input name="content" type="text" />
+            <input
+              name="content"
+              type="text"
+              value={this.state.content}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <label htmlFor="imageUrl">
               <small>Image (optional)</small>
             </label>
-            <input name="imageUrl" type="text" />
+            <input
+              name="imageUrl"
+              type="text"
+              value={this.state.imageUrl}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <label htmlFor="imageAlt">
               <small>Image Alt Text (optional)</small>
             </label>
-            <input name="imageAlt" type="text" />
+            <input
+              name="imageAlt"
+              type="text"
+              value={this.state.imageAlt}
+              onChange={this.handleChange}
+            />
           </div>
           <div>
             <label htmlFor="tags">
               <small>Tags (seperate with a comma and space)</small>
             </label>
-            <input name="tags" type="text" />
+            <input
+              name="tags"
+              type="text"
+              value={this.state.tags}
+              onChange={this.handleChange}
+            />
           </div>
         </form>
       </div>
@@ -81,4 +114,10 @@ class StoryEntry extends React.Component {
   }
 }
 
-export default StoryEntry
+const mapDispatchToProps = dispatch => {
+  return {
+    addNewStory: story => addNewStoryThunk(story)
+  }
+}
+
+export default connect(null, mapDispatchToProps)(StoryEntry)
