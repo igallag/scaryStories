d4eb9181e9e5de38483ef673b6e5d326360c5bd6
Ian Gallgher <igallag93@gmail.com>
2019-11-18 14:46:01 -0500

    added a delete button that only shoes up if the user is the one that wrote the story

--- client/components/Story/SingleStoryView.js
+++ client/components/Story/SingleStoryView.js
@@ -2,17 +2,21 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 
-// This is not correctly displaying the story despite it working
-// only moments ago
-
 const SingleStoryView = props => {
   const currStory = props.stories[props.location.state.storyId - 1]
-  console.log('INSIDE SINGLE STORY VIEW')
+  console.log(props.user, 'this is props.user')
   return currStory ? (
     <div>
       <img src={currStory.imageUrl} height="200" width="200" />
       <h1>{currStory.title}</h1>
       <p>{currStory.content}</p>
+      <div>
+        {currStory.userId === props.user.id ? (
+          <button type="button">Delete</button>
+        ) : (
+          ''
+        )}
+      </div>
     </div>
   ) : (
     <p>Loading</p>
@@ -21,7 +25,8 @@ const SingleStoryView = props => {
 
 const mapStateToProps = state => {
   return {
-    stories: state.story.stories
+    stories: state.story.stories,
+    user: state.user
   }
 }
 
