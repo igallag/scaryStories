5bf6636d4e831943ae85fb501c81e33804412b61
Ian Gallgher <igallag93@gmail.com>
2019-11-19 16:51:25 -0500

    added a button to be filled out later

--- client/components/User/UserSettings.js
+++ client/components/User/UserSettings.js
@@ -2,12 +2,18 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 const UserSetting = props => {
+  console.log(props, 'This is props')
+
+  const {user} = props
   return (
     <div>
       <h2>Settings</h2>
       <div>
         <h3>Your Info</h3>
-        <p />
+        <p>{user.email}</p>
+        <button type="button" onClick={() => alert('build me!')}>
+          Edit Information
+        </button>
       </div>
     </div>
   )
