2589b6777ea2a5ffd8e04f364753c42eb09b6cb8
Ian Gallgher <igallag93@gmail.com>
2019-11-22 20:20:26 -0500

    wrote a few test elements to style and added a bit to the stylecss

--- client/components/User/DarkMode.js
+++ client/components/User/DarkMode.js
@@ -2,7 +2,15 @@ import React from 'react'
 import {connect} from 'react-redux'
 
 const DarkMode = props => {
-  return <h1>Welcome to DarkMode</h1>
+  return (
+    <div className="night-container">
+      <h1 className="night-text">Welcome to DarkMode</h1>
+      <p>
+        I am thinking that I can put some random things and maybe some D3
+        chicanery and some instructions
+      </p>
+    </div>
+  )
 }
 
 export default DarkMode
