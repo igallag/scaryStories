e55cf44410c54690092530a4282fd1dfacb2bb6f
Ian Gallgher <igallag93@gmail.com>
2020-02-19 21:28:31 -0500

    added react d3 to the tag graph page

--- client/components/Stats/TagGraphs.js
+++ client/components/Stats/TagGraphs.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import * as d3 from 'd3'
+import rd3 from 'react-d3-library'
 
 // const Graph = props => {
 //   return (
